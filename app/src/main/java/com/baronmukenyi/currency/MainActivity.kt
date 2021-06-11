da1621a623a4fc343e550e5ea25a5fc335ce0aef
MercyRise <baronmukenyi@gmail.com>
2021-06-11 19:47:19 +0200

    added import

--- app/src/main/java/com/baronmukenyi/currency/MainActivity.kt
+++ app/src/main/java/com/baronmukenyi/currency/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.ui.core.setContent
+
 import com.baronmukenyi.currency.composable.ConverterScreen
 import com.baronmukenyi.currency.ui.theme.CurrencyTheme
 
