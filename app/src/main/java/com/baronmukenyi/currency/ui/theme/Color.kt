20dd70ea14c88c0f8dab8323f644b15e56e06245
MercyRise <baronmukenyi@gmail.com>
2021-06-11 12:20:36 +0200

    minor changes

--- app/src/main/java/com/baronmukenyi/currency/ui/theme/Color.kt
+++ app/src/main/java/com/baronmukenyi/currency/ui/theme/Color.kt
@@ -1,5 +1,10 @@
 package com.baronmukenyi.currency.ui.theme
 
-import android.graphics.Color
 
+import androidx.compose.ui.graphics.Color
 
+val Orange_700 = Color(0xFFF24F09)
+val White_900 = Color(0xFFFFFFFF)
+
+val Label_Color = Color(0xFF8F8F8F)
+val Value_Color = Color(0xFF444444)
