355bd9be7e357d50c9b582cb007cf32e6bda1afb
MercyRise <baronmukenyi@gmail.com>
2021-06-11 19:13:38 +0200

    added import

--- app/src/main/java/com/baronmukenyi/currency/MainActivity.kt
+++ app/src/main/java/com/baronmukenyi/currency/MainActivity.kt
@@ -2,10 +2,19 @@ package com.baronmukenyi.currency
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.ui.core.setContent
+import com.baronmukenyi.currency.composable.ConverterScreen
+import com.baronmukenyi.currency.ui.theme.CurrencyTheme
 
 class MainActivity : AppCompatActivity() {
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent { 
+            CurrencyTheme { 
+                ConverterScreen()
+            }
+        }
     }
 }
